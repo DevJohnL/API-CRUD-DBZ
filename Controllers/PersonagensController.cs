2da950210eeb7aca6797afca8021fb1986b4ca47
João Lucas Gifoni <jl109160@gmail.com>
2026-05-16 12:24:05 -0300

    Feat: Enhance PersonagemController with CRUD operations and validation for Personagem model

--- Controllers/PersonagensController.cs
+++ Controllers/PersonagensController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using ProjetoDBZ.Data;
 using ProjetoDBZ.Models;
 
@@ -16,13 +17,70 @@ namespace ProjetoDBZ.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddPersonagem(Personagem personagem)
+        public async Task<IActionResult> AddPersonagem([FromBody] Personagem personagem)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
             _appDbContext.DBZ.Add(personagem);
 
             await _appDbContext.SaveChangesAsync();
-
-            return Ok($"Personagem criado: {personagem}");
+            
+            return Created ("Personagem adicionado com sucesso:", personagem);
         }
+
+        [HttpGet]
+        public async Task<ActionResult<IAsyncEnumerable<Personagem>>> GetPersonagens()
+        {
+            var personagens = await _appDbContext.DBZ.ToListAsync();
+
+            return Ok (personagens);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Personagem>> GetPersonagem(int id)
+        {
+            var personagem = await _appDbContext.DBZ.FindAsync(id);
+            if (personagem == null)
+            {
+                return NotFound("Personagem não encontrado!");
+            }
+
+            return Ok (personagem);
+        }
+
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdatePersonagem(int id, [FromBody] Personagem personagemAtualizado)
+        {
+            var personagemExistente = await _appDbContext.DBZ.FindAsync(id);
+            if (personagemExistente == null)
+            {
+                return NotFound("Personagem não encontrado!");
+            }
+
+            _appDbContext.Entry(personagemExistente).CurrentValues.SetValues(personagemAtualizado);
+
+            await _appDbContext.SaveChangesAsync();
+            return StatusCode (201, personagemExistente);
+        }
+        
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeletePersonagem(int id)
+        {
+            var personagem = await _appDbContext.DBZ.FindAsync(id);
+            if (personagem == null)
+            {
+                return NotFound("Personagem não encontrado!");
+            }
+
+            _appDbContext.DBZ.Remove(personagem);
+
+            await _appDbContext.SaveChangesAsync();
+            return Ok ("Personagem Deletado com sucesso!");
+        }
+
+
+    
     }
 }
